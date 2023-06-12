91d325428762ce9f0fb24a291909114929d3e70f
Samuel <samuelou510@gmail.com>
2023-06-12 10:59:19 -0400

    revised string practice with notes

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 // Write your code in this file!
 let currentUser = "sam";
 const welcomeMessage = "Welcome to Flatbook, " + currentUser + "!";
-
+// can also interpolate like 'Welcome to Flatbook, ${currentUser}!';
+// must not use double quotes for interpolation
 const excitedWelcomeMessage =
   'WELCOME TO FLATBOOK, ' + currentUser.toUpperCase() + '!';
 
